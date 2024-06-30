ffeb4da6b39724ff08cbbf4142f38c15decb6c1d
Dmitry <58977309+Mon4ik@users.noreply.github.com>
2024-06-30 19:19:52 +0300

    BREAKING CHANGE: new design

---
+++ components/ProjectCard.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+  description: string
+  link: string
+}>()
+</script>
+
+<template>
+  <div class="project h-[50vh]">
+    <div class="project-info">
+      <h2 class="project-name">{{ props.name }}</h2>
+      <p class="project-desc">{{ props.description }}</p>
+    </div>
+    <div class="project-see">
+      <NuxtLink :to="props.link" class="w-full h-full">
+        <button class="btn project-see-btn">See</button>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.project {
+  @apply w-full p-5;
+  @apply rounded-3xl border border-gray;
+
+  @apply flex flex-col gap-2;
+
+  .project-info {
+    @apply flex-grow;
+
+    .project-name {
+      @apply text-3xl;
+    }
+
+    .project-desc {
+      @apply text-2xl italic;
+    }
+  }
+
+  .project-see {
+    @apply h-16 w-full;
+
+    .project-see-btn {
+      @apply w-full h-full;
+      @apply bg-red text-white font-semibold;
+    }
+  }
+}
+</style>
